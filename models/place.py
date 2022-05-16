d41754e30ab3984e4a6c7f2029055f2e5add66f8
angelicarm3 <angie00069@gmail.com>
2022-05-16 10:45:25 -0500

    5

--- models/place.py
+++ models/place.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import relationship
 
 place_amenity = Table('place_amenity', Base.metadata, Column(
     'place_id', String(60), ForeignKey('places.id'), primary_key=True, nullable=False), Column(
-        'amenity_id', String(60), ForeignKey('amenities_id'), primary_key=True, nullable=False))
+        'amenity_id', String(60), ForeignKey('amenities.id'), primary_key=True, nullable=False))
 
 
 class Place(BaseModel, Base):

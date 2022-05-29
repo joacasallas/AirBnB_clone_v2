2f12a73c4407264aeee3fd9bc77fba884303cd55
joacasallas <joacasallas@gmail.com>
2022-05-28 23:21:40 -0500

    update

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     """display HTML page with the list of all State objects present"""
-    states = storage.all(State).values()
+    states = storage.all("State").values()
     return render_template("7-states_list.html", states=states)
 
 

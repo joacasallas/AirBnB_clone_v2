080704a8eaf3d92efc4c339a4423071738ce24ba
joacasallas <joacasallas@gmail.com>
2022-06-11 00:00:10 -0500

    update

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,13 +1,35 @@
 #!/usr/bin/env bash
-# Bash Script to Setup Airbnb Static_web 
-sudo apt-get update -y
-sudo apt-get upgrade -y
+# Sets up a web server for deployment of web_static.
+
+sudo apt-get update
 sudo apt-get install -y nginx
-sudo mkdir -p /data/web_static/
+
 sudo mkdir -p /data/web_static/releases/test/
-sudo mkdir /data/web_static/shared/
-sudo echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html
-sudo ln -sf /data/web_static/releases/test /data/web_static/current 
-sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+sudo mkdir -p /data/web_static/shared/
+echo "Holberton School" > /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+sudo chown -R ubuntu /data/
+sudo chgrp -R ubuntu /data/
+
+printf %s "server {
+    listen 80;
+    listen [::]:80 default_server;
+    add_header X-Served-By $HOSTNAME;
+    root   /etc/nginx/html;
+    index  index.html index.htm;
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
+    location /redirect_me {
+        return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+    }
+    error_page 404 /404.html;
+    location /404 {
+      root /etc/nginx/html;
+      internal;
+    }
+}" > /etc/nginx/sites-available/default
+
 sudo service nginx restart
